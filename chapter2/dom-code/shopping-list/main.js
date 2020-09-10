9b6eed145fe0409b88539d098dcb9769ed12dac9
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-09-10 14:21:21 +0900

    JS: shopping-list(ing)

--- chapter2/dom-code/shopping-list/main.js
+++ chapter2/dom-code/shopping-list/main.js
@@ -1,13 +1,26 @@
 'use strict';
 
-const listItem = document.querySelector('.list__item');
 const listText = document.querySelector('.list__text');
 const listDel = document.querySelector('.list__del');
 const input = document.querySelector('input');
-const add = document.querySelector('.btn__add');
+const btnAdd = document.querySelector('.btn__add');
+const log = document.getElementById('values');
 
-input.addEventListener('input');
+input.addEventListener('change', updateValue);
+btnAdd.addEventListener('click', updateValue);
+function updateValue(e) {
+  if (input.value == null || '') {
+    return;
+  }
+  console.log(e.target.value || input.value);
+  createList();
+  input.value = null;
+}
 
-function updateValue(event) {
-  console.log(event.target.value);
+function createList() {
+  const list = document.querySelector('.list');
+  const listItem = document.createElement('div');
+  listItem.setAttribute('class', 'list__item');
+  listItem.textContent = 'good';
+  list.appendChild(listItem);
 }
