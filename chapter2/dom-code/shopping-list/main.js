db477dd69c5284947b21498507a0ee2b1c36ef23
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-09-15 16:38:02 +0900

    JS: self-end

--- chapter2/dom-code/shopping-list/main.js
+++ chapter2/dom-code/shopping-list/main.js
@@ -1,23 +1,26 @@
 'use strict';
 const list = document.querySelector('.list');
-const listText = document.querySelector('.list__text');
-const listDel = document.querySelector('.list__del');
 const input = document.querySelector('input');
 const btnAdd = document.querySelector('.btn__add');
 
-input.addEventListener('change', updateValue);
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    updateValue(e);
+  }
+});
 btnAdd.addEventListener('click', updateValue);
 
 function updateValue(e) {
-  if (input.value == null || '') {
+  if (input.value == null || input.value == '') {
     return;
   }
   const value = e.target.value || input.value;
   createList(value);
   input.value = null;
 }
+
 function createList(value) {
-  const newListItem = document.createElement('div');
+  const newListItem = document.createElement('li');
   const newListText = document.createElement('p');
   const newListDel = document.createElement('span');
   newListItem.appendChild(newListText);
@@ -28,9 +31,9 @@ function createList(value) {
   newListText.textContent = value;
   newListDel.innerHTML = '<i class="far fa-trash-alt"></i>';
   list.appendChild(newListItem);
+  newListDel.addEventListener('click', (event) => {
+    const Btn = event.target;
+    const li = Btn.parentNode.parentNode;
+    list.removeChild(li);
+  });
 }
-
-listDel.addEventListener('click', (event) => {
-  const li = event.target.parentNode.parentNode;
-  list.removeChild(li);
-});
