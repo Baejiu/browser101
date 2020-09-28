e4359c15e87a890646c4e1c73f4835aa9e08849c
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-09-28 14:40:26 +0900

    carrot game-self:end?

--- chapter3/carrot-self/main.js
+++ chapter3/carrot-self/main.js
@@ -6,13 +6,27 @@ const remainder = document.querySelector('.header__remainder');
 const replayPop = document.querySelector('.game__replay');
 const resultWin = document.querySelector('.game__result--win');
 const resultLost = document.querySelector('.game__result--lost');
-const replayBtn = document.querySelector('.replay-btn');
+const replayBtn = document.querySelectorAll('.replay-btn');
+const imgGame = document.querySelector('.ing-btn');
 const timerBox = document.querySelector('.header__timer');
+let bgSound = new Audio('./sound/bg.mp3');
+let alertSound = new Audio('./sound/alert.wav');
+let bugSound = new Audio('./sound/bug_pull.mp3');
+let carrotSound = new Audio('./sound/carrot_pull.mp3');
+let winSound = new Audio('./sound/game_win.mp3');
 let timer = 9;
 let count = 10;
-
+let carrotNumber = 10;
+let bugNumber = 10;
+for (var i = 0; i < replayBtn.length; i++) {
+  var item = replayBtn.item(i);
+  item.addEventListener('click', () => {
+    gameReStart();
+  });
+}
 headerBtn.addEventListener('click', () => {
   if (timer <= 8) {
+    alertSound.play();
     headerBtn.innerHTML = `<i class="fas fa-play">`;
     replayPop.style.display = 'block';
     timerStop();
@@ -20,7 +34,20 @@ headerBtn.addEventListener('click', () => {
     gameStart();
   }
 });
-
+imgGame.addEventListener('click', () => {
+  replayPop.style.display = 'none';
+  timerStart();
+});
+function gameReStart() {
+  resultWin.style.display = 'none';
+  resultLost.style.display = 'none';
+  replayPop.style.display = 'none';
+  game.innerHTML = '';
+  timer = 10;
+  carrotNumber = 10;
+  bugNumber = 10;
+  gameStart();
+}
 function gameStart() {
   count = 10;
   remainder.innerHTML = `${count}`;
@@ -31,12 +58,15 @@ function gameStart() {
     createBug();
   }
 }
+
 function gameWin() {
   timerStop();
+  winSound.play();
   resultWin.style.display = 'block';
 }
 function gameOver() {
   timerStop();
+  alertSound.play();
   resultLost.style.display = 'block';
 }
 function changeBtn() {
@@ -45,30 +75,30 @@ function changeBtn() {
 let interval;
 function timerStart() {
   interval = setInterval(() => {
-    timerBox.innerHTML = `00: ${timer}`;
     if (timer === 0) {
       gameOver();
       return;
     }
-    timer--;
     if (count == 0) {
       gameWin();
     }
+    timerBox.innerHTML = `00: ${timer}`;
+    timer--;
   }, 1000);
 }
 function timerStop() {
   clearInterval(interval);
 }
 
-let id = 10;
 function createcarrot() {
-  if (id === 0) {
+  if (carrotNumber === 0) {
     return;
   }
   const carrot = document.createElement('div');
   carrot.setAttribute('class', 'carrot game__item');
-  carrot.setAttribute('data-id', id);
+  carrot.setAttribute('data-id', carrotNumber);
   carrot.addEventListener('click', (e) => {
+    carrotSound.play();
     const target = e.target;
     count--;
     remainder.innerHTML = `${count}`;
@@ -87,19 +117,19 @@ function createcarrot() {
   carrot.style.top = `${getRandomheight(1, gameHeight)}px`;
   carrot.style.left = `${getRandomwidth(1, gameWidth)}px`;
 
-  id--;
+  carrotNumber--;
   return carrot;
 }
 
-let id2 = 10;
 function createBug() {
-  if (id2 === 0) {
+  if (bugNumber === 0) {
     return;
   }
   const bug = document.createElement('div');
   bug.setAttribute('class', 'bug game__item');
-  bug.setAttribute('data-id', id2);
+  bug.setAttribute('data-id', bugNumber);
   bug.addEventListener('click', (e) => {
+    bugSound.play();
     const target = e.target;
     gameOver();
   });
@@ -116,6 +146,7 @@ function createBug() {
   bug.style.top = `${getRandomheight(1, gameHeight)}px`;
   bug.style.left = `${getRandomwidth(1, gameWidth)}px`;
 
-  id2--;
+  bugNumber--;
   return bug;
 }
+bgSound.play();
