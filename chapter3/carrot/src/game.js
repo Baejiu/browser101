d73926973d1258458581824ccf0a9b49bd020b11
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-10-15 12:16:10 +0900

    carrot :+1:

--- chapter3/carrot/src/game.js
+++ chapter3/carrot/src/game.js
@@ -1,7 +1,14 @@
 'use strict';
+import { Field, ItemType } from './field.js';
+
 import * as sound from './sound.js';
-import Field from './field.js';
-export default class GameBuilder {
+export const Reason = Object.freeze({
+  win: 'win',
+  lose: 'lose',
+  cancel: 'cancel',
+});
+
+export class GameBuilder {
   gameDuration(duration) {
     this.gameDuration = duration;
     return this;
@@ -30,7 +37,7 @@ class Game {
     this.gameBtn = document.querySelector('.game__button');
     this.gameBtn.addEventListener('click', () => {
       if (this.started) {
-        this.stop();
+        this.stop(Reason.cancel);
       } else {
         this.start();
       }
@@ -50,14 +57,14 @@ class Game {
     if (!this.started) {
       return;
     }
-    if (item === 'carrot') {
+    if (item === ItemType.carrot) {
       this.score++;
       this.updateScoreBoard();
       if (this.score === this.carrotCount) {
-        this.finish(true);
+        this.stop(Reason.win);
       }
-    } else if (item === 'bug') {
-      this.finish(false);
+    } else if (item === ItemType.bug) {
+      this.stop(Reason.lose);
     }
   };
 
@@ -70,27 +77,12 @@ class Game {
     sound.playBackground();
   }
 
-  stop() {
+  stop(reason) {
     this.started = false;
     this.stopGameTimer();
     this.hideGameButton();
-
-    sound.playAlert();
     sound.stopBackground();
-    this.onGameStop && this.onGameStop('cancel');
-  }
-
-  finish(win) {
-    this.started = false;
-    this.hideGameButton();
-    if (win) {
-      sound.playWin();
-    } else {
-      sound.playBug();
-    }
-    this.stopGameTimer();
-    sound.stopBackground();
-    this.onGameStop && this.onGameStop(win ? 'win' : 'lose');
+    this.onGameStop && this.onGameStop(reason);
   }
 
   showStopButton() {
@@ -112,7 +104,7 @@ class Game {
     this.timer = setInterval(() => {
       if (remainingTimeSec <= 0) {
         clearInterval(this.timer);
-        this.finish(this.carrotCount === this.score);
+        this.stop(this.carrotCount === this.score ? Reason.win : Reason.lose);
         return;
       }
       this.updateTimerText(--remainingTimeSec);
--- chapter3/carrot/src/main.js
+++ chapter3/carrot/src/main.js
@@ -1,26 +1,30 @@
 'use strict';
 
-import GameBuilder from './game.js';
+import { GameBuilder, Reason } from './game.js';
 import PopUp from './popup.js';
+import * as sound from './sound.js';
 
 const gameFinishBanner = new PopUp();
 const game = new GameBuilder()
-  .gameDuration(3)
-  .carrotcount(3)
-  .bugCount(3)
+  .gameDuration(10)
+  .carrotcount(10)
+  .bugCount(10)
   .build();
 
 game.setGameStopListener((reason) => {
   let message;
   switch (reason) {
-    case 'cancel':
+    case Reason.cancel:
       message = 'Replay?';
+      sound.playAlert();
       break;
-    case 'win':
+    case Reason.win:
       message = 'You Won!';
+      sound.playWin();
       break;
-    case 'lose':
+    case Reason.lose:
       message = 'You Lost~';
+      sound.playBug();
       break;
     default:
       throw new Error('not valid reason');
